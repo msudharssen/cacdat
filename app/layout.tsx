d234a7a98fd9b4b4d1da16cc36071b069efa26d4
Sudharssen Murali <m.sudharssen@gmail.com>
2026-01-13 15:15:42 -0500

    Initial Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Schibsted_Grotesk } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted",
   subsets: ["latin"],
 });
 
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${schibstedGrotesk.variable} antialiased`}
       >
         {children}
       </body>
