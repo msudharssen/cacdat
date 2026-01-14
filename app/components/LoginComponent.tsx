62d1248d3393d7e4c74af0b2cf2b9997bf38be2a
Sudharssen Murali <m.sudharssen@gmail.com>
2026-01-13 22:58:04 -0500

    Added register and login function and updated firebase configuration

--- app/components/LoginComponent.tsx
+++ app/components/LoginComponent.tsx
@@ -12,6 +12,9 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
 import { BiSolidError } from "react-icons/bi";
+import {auth} from "@/firebaseconfig"
+import { useRouter } from "next/navigation"
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, getIdToken } from "firebase/auth"
 
 
 export default function LoginComponent() {
@@ -19,9 +22,48 @@ export default function LoginComponent() {
   const[email, setEmail] =useState<string>("")
   const[password, setPassword]=useState<string>("")
   const[repeatPassword, setRepeatPassword]=useState<string>("")
+  const[loginError, setLoginError]=useState<string>("")
+  const router = useRouter()
+
   function switchRegisterFlag(){
     setRegisterFlag(!registerFlag)
   }
+  async function handleLogin() {
+    setLoginError("")
+    
+    signInWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        const user = userCredential.user
+        if (user) {
+          const token = await getIdToken(user)
+          console.log("Successfully Logged In",user)
+          
+          // Fetch health information after login        
+          router.push('/dashboard')      
+        }
+      })
+      .catch((error) => {
+        console.log("Login Error: ", error.message)
+      })
+  }
+  async function handleRegister(){
+    
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential:any)=>{
+        const user = userCredential.user
+        if(user){
+          console.log("Successfully Registered: ", user)
+        }
+      })
+      .catch((error:any) =>{
+        const errorCode = error.code
+        const errorMessage = error.message;
+        console.log("Error Code: ", errorCode)
+        console.log("Error Message: ", errorMessage)
+      })
+    }
+
+
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
@@ -32,7 +74,9 @@ export default function LoginComponent() {
           Enter a valid email address and password to register
         </CardDescription>}
         <CardAction>
-          <Button variant="link" className="cursor-pointer" onClick={switchRegisterFlag}>Sign Up</Button>
+          {!registerFlag ? <Button variant="link" className="cursor-pointer" onClick={switchRegisterFlag}>Sign Up</Button> :
+                    <Button variant="link" className="cursor-pointer" onClick={switchRegisterFlag}>Log In</Button>
+        }
         </CardAction>
       </CardHeader>
       <CardContent>
@@ -61,7 +105,7 @@ export default function LoginComponent() {
               </div>
               {!registerFlag ? <Input id="password" type="password" required /> :
               <><Input id="password" onChange={(e=>{setPassword(e.target.value)})}type="password" required />
-              <Label>Re-Enter Password</Label><Input id="password" onChange={(e=>{setRepeatPassword(e.target.value)})}type="password" required /></> }
+              <Label>Re-Enter Password</Label><Input id="password2" onChange={(e=>{setRepeatPassword(e.target.value)})}type="password" required /></> }
             </div>
             {password!==repeatPassword ? <p className="text-red-500 flex">Passwords Do Not Match!<BiSolidError className="text-l"/></p> : null}
           </div>
@@ -69,9 +113,9 @@ export default function LoginComponent() {
       </CardContent>
       <CardFooter className="flex-col gap-2">
 
-        {!registerFlag? <Button type="submit" className="w-full cursor-pointer">
+        {!registerFlag? <Button type="submit" onClick={handleLogin} className="w-full cursor-pointer">
           Login
-        </Button> : <Button type="submit" className="w-full cursor-pointer">
+        </Button> : <Button onClick={handleRegister} type="submit" className="w-full cursor-pointer">
           Register
         </Button>}
         
